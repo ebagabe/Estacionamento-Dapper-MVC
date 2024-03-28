7714983dea85a35bbedee0aa4d08aa6ff358bb8b
ebagabe <gabriel.ebagabe@gmail.com>
2024-03-28 19:53:27 -0300

    Initial Commit

---
+++ estacionamento/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace estacionamento_dapper.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
