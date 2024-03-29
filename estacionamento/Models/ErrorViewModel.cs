dc33e738b80e4ea83548f35f76f794f7ddf108e5
ebagabe <gabriel.ebagabe@gmail.com>
2024-03-28 21:15:05 -0300

    Criando Models e Excluindo arquivos desnecessarios

--- estacionamento/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace estacionamento_dapper.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
